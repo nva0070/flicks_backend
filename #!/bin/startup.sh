634ff6f3fb521e199de58610ad88cde8875bde4c
Chinmay Ajith <72075630+chimnayajith@users.noreply.github.com>
2025-06-10 20:25:07 +0530

    Create startup.sh

---
+++ #!/bin/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python manage.py showmigrations
+python manage.py makemigrations products
+python manage.py migrate
+python manage.py shell < create_superuser.py
+gunicorn flicks.wsgi:application
